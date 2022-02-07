75c6677497fd86ec5f80c066757fefb8a895bc20
ilonaverenich <35536824+ilonaverenich@users.noreply.github.com>
2022-02-07 16:10:01 +0300

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React, {useState,Fragment} from 'react';
 import './App.css';
 
-function App() {
+
+const Components =()=>{
+  const [text,setText]= useState('');
+  const valid = (e)=>{
+    e.length<5 ?  setText(e): setText('error')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        {text}
+        <input onChange={(event)=>valid(event.target.value)}value = {text}/>
+    </>
+  )}
+
+
+function App() {
+
+  return (
+      <div>
+          <Components name={'Ilona'}/>
+      </div>
   );
 }
 
